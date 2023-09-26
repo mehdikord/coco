e29e3bf8d8e7b060bf76cc62d9d77f3a66f3eca4
mehdikord <mehdi.kord171@gmail.com>
2023-09-26 21:56:29 +0330

    update front

--- resources/js/front/template/footer/Front_Template_Footer_Mobile.vue
+++ resources/js/front/template/footer/Front_Template_Footer_Mobile.vue
@@ -7,14 +7,14 @@
         <a href="wishlist.html" title="Wishlist" class="pt-3">
             <i class="fas fa-heart font-20"></i><span>برگزیده</span><sup>0</sup>
         </a>
-        <router-link :to="{name : 'index'}" title="Home Page" class="pt-3 text-success" >
-            <i class="fas fa-home font-20"></i><span>کوکوکافی</span>
+        <router-link :to="{name : 'index'}" title="صفحه اصلی" class="pt-3" :class="{'bg-grey-3' : this.$route.name === 'index'}">
+            <i class="fas fa-home font-20 " :class="{'text-success' : this.$route.name === 'index'}"></i><span :class="{'text-success' : this.$route.name === 'index'}">کوکوکافی</span>
         </router-link>
-        <button class="cate-btn pt-3" title="Category List">
-            <i class="fas fa-shopping-cart font-20"></i><span>محصولات</span>
-        </button>
-        <button class="cart-btn pt-3" title="Cartlist">
-            <i class="fas fa-shopping-basket font-20"></i><span>سبد‌خرید</span><sup>9+</sup>
+        <router-link :to="{name : 'shop'}" class="cate-btn pt-3" title="محصولات فروشگاه" :class="{'bg-grey-3' : this.$route.name === 'shop'}">
+            <i class="fas fa-shopping-cart font-20" :class="{'text-success' : this.$route.name === 'shop'}"></i><span :class="{'text-success' : this.$route.name === 'shop'}">محصولات</span>
+        </router-link>
+        <button class="cart-btn pt-3" title="سبد خرید شما">
+            <i class="fas fa-shopping-basket font-20"></i><span>سبد‌خرید</span><sup>{{this.CartItemCount}}</sup>
         </button>
     </div>
 </template>
